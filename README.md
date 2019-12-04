20b6d2ea73eaec42526dcaebcb04c6b934b1d7b3
Sean Lindsey <slindsey@chapman.edu>
2019-12-03 21:48:13 -0800

    Adds cars.sh, README.md, and My_old_cars

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 This repo contains a bash script named “cars.sh” that will help a user maintain an inventory of old cars.
  
  The user will be presented with a menu of three options:
+
+Option 1: Add a car to the list
+	it will ask for year make and model, and it will format the inputs into a sting in this format
+		YEAR:MAKE:MODEL
+Option 2: Sort and view the current list of cars
+	sorts the list of cars by year, going from oldest to new
+Option 3: Quit
+	quits the program
--- cars.sh
+++ cars.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 #cars.sh
 #Sean Lindsey
-
-while true;
+while :
 do
 	echo "Would you like to 1.Add a car 2.List cars in inventory 3.Quit"
-	read $CHOICE
-	if [[ $CHOICE = 1 ]]
-	then
-		echo "What is the year?"
-		read $YEAR
-		echo"What is the Make?"
-		read $MAKE
-		echo "What is the Model?"
-		read $MODEL
-		echo "$YEAR:$MAKE:$MODEL" >> My_old_cars.txt
-	elif [[ $CHOICE = 2 ]]
-	then
-		sort My_old_cars.txt
-	elif [[ $CHOICE = 3 ]]
-	then
-		echo "GOODBYE"
-		continue
-	break
-	fi
+	read -r CHOICE
+	case "$CHOICE" in 
+		"1") echo "Adding Car"
+			echo "What is the year?"
+			read -r YEAR
+			echo "What is the Make?"
+			read -r MAKE
+			echo "What is the Model?"
+			read -r MODEL
+			printf "$YEAR:$MAKE:$MODEL\n" >> My_old_cars
+			;;
+		"2") echo "Listing Car"
+			sort My_old_cars
+			;;
+		"3") echo "Quitting"
+			echo "GOODBYE"
+			break
+			;;
+	esac
 done
-
 
